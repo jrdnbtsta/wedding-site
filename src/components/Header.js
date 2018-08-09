872f1bcdf55770038af5103d92d56b17f51188a7
Jordan Bautista <jordanbau@gmail.com>
2018-08-09 09:51:08 -0700

    background, changed gradient

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,18 @@ const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Carey & Jordan</h1>
+                <p>Kauai 2019</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Wedding Details</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Wedding</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>Planning</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Traveling</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Guide</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>Photos</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>RSVP</a></li>
             </ul>
         </nav>
     </header>
